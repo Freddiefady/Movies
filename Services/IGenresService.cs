975e0e9eb6d55a668ea022ed7918feb043ce719d
Freddiefady <dodofady49@gmail.com>
2025-12-16 10:05:22 +0200

    chore: Interface for Genre service.

---
+++ Services/IGenresService.cs
@@ -0,0 +1,22 @@
+﻿namespace Movies.Services
+{
+    public interface IGenresService
+    {
+		// Retrieves all genres asynchronously.
+		Task<IEnumerable<Genre>> GetAll();
+
+		// Retrieves a genre by its ID asynchronously.
+		Task<Genre?> GetByIdAsync(byte id);
+
+		// Adds a new genre asynchronously.
+		Task<Genre> AddAsync(Genre genre);
+
+		// Updates an existing genre asynchronously.
+		Genre UpdateAsync(Genre genre);
+
+		// Deletes a genre by its ID asynchronously.
+		Genre DeleteAsync(Genre genre);
+
+        Task<bool> isVaildGenre(byte id);
+	}
+}
