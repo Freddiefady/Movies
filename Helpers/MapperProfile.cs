b0cc675df19909827b9daed4a6a1811ae6057f24
Freddiefady <dodofady49@gmail.com>
2025-12-17 10:47:20 +0200

    chore: Add config auto mapper package.

---
+++ Helpers/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Movies.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Movie, MovieDto>();
+        }
+    }
+}
